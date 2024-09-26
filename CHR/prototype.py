86953c5cad333ee0c16f8626c80563a940094073
Artem Yakhontov <72457086+Setet@users.noreply.github.com>
2024-09-26 15:26:09 +0300

    v1.01

--- CHR/prototype.py
+++ CHR/prototype.py
@@ -1,28 +1,15 @@
-def cover_with_stars(hypergraph):
-    """ Реализует задачу о покрытии гиперграфа звездочками.
-        Аргументы: гиперграф (dict): словарь, представляющий гиперграф. Ключами являются вершины,
-    значениями - наборы ребер, к которым принадлежит вершина.
-        Возвращает: список: список звездочек, покрывающих гиперграф.  """
-    stars = []
-    uncovered_edges = {frozenset(edge) for edge in hypergraph.values()}
+import hypernetx as hnx
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import networkx as nx
+import warnings
 
-    # Выбираем ребро с максимальным количеством непокрытых вершин
-    while uncovered_edges:
-        max_edge = max(uncovered_edges, key=lambda x: len(x - (x.union(*stars))))
-        star = set(max_edge)
-        # Добавьте ребро к звезде и удалите ее с незакрытых рёбер
-        stars.append(star)
-        uncovered_edges.remove(max_edge)
+warnings.simplefilter('ignore')
 
-    return stars
+list_of_lists = [['book', 'candle', 'cat'], ['book', 'coffee cup'], ['coffee cup', 'radio']]
 
+H = hnx.Hypergraph(list_of_lists)
 
-hypergraph = {'A': {'A1', 'A2', 'A3'},
-              'B': {'A1', 'B2', 'B3'},
-              'C': {'B2', 'C3', 'C4'},
-              'D': {'C3', 'D4', 'D5'}, }
-
-stars = cover_with_stars(hypergraph)
-print("Звезды, покрывающие гиперграф:")
-for i, star in enumerate(stars):
-    print(f"Звезда {i + 1}: {star}")
+plt.subplots(figsize=(4, 4))
+hnx.draw(H)
